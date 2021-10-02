764dcab7392b8aa2eb88877ffedfd27808d197b4
jyotip31 <91590380+jyotip31@users.noreply.github.com>
2021-10-02 10:43:06 +0530

    Revert "rotate_screen_jyotip31"

--- scripts/Hello_World_Nihal-Priyadarshi.py
+++ scripts/Hello_World_Nihal-Priyadarshi.py
@@ -1,8 +1,3 @@
-#pip install rotate-screen
+import sys
 
-import rotatescreen
-import time
-screen = rotatescreen.get_primary_display()
-for i in range(4):
-    time.sleep(3)
-    screen.rotate_to(i*90 % 360)
+print("Hello World")
